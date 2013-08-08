682c2013ef6e17aeeec5b7d505c68d8d20960629
Michael Kiening <michael@kiening-online.de>
2013-08-08 10:35:52 +0000

    pack scrip finalized

--- pack.sh
+++ pack.sh
@@ -1,11 +1,14 @@
+make clean
+make distclean
+svn-clean
 aclocal
 autoconf
 automake -a
 automake
-#./configure
-./configure --prefix=/var/tmp/kiening/SomeNA
+./configure
+#./configure --prefix=/var/tmp/kiening/SomeNA
 make
-#make dist
-#mv 
-#debuild --lintian-opts --pedantic 
+make dist
+mv somena-1.0.1-1.tar.gz ../somena_1.0.1.tar.gz 
+debuild --lintian-opts --pedantic 
 #make install
