b6ef1d4edd31f791c21852a94d270a76cc33e65e
Christian Dallago <sacdallago@users.noreply.github.com>
2016-02-01 14:56:22 +0100

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,12 +1,12 @@
 make clean
 make distclean
-svn-clean
+# svn-clean
 aclocal
 autoconf
 automake -a
 automake
 ./configure
-#./configure --prefix=/var/tmp/kiening/SomeNA
+# ./configure --prefix=/var/tmp/kiening/SomeNA
 make
 make dist
 mv somena-1.0.1-1.tar.gz ../somena_1.0.1.tar.gz 
